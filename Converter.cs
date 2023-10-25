66672e73d05b7236bad0842a6c9ce42ddaf58497
Balogun Muhammed-Awwal <77177481+balogun14@users.noreply.github.com>
2023-10-25 10:13:13 +0000

    Added some changes

--- Converter.cs
+++ Converter.cs
@@ -1,33 +1,55 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TemperatureConverter
 {
     public class Converter
     {
-        static void TemperatureConverter() 
+       public static void TemperatureConverter()
+{
+        Console.Write("Choose an option\n1. Convert from Celsius to Fahrenheit.\n2. Convert from Fahrenheit to Celsius\n3.Exit: ");
+        int option = int.Parse(Console.ReadLine()!);
+        switch(option)
         {
-            Console.WriteLine("=======This is a simple app that converts temperatures between Celsius and Fahrenheit======");
-            Console.Write("Choose an option\n \t1. Convert from Celsius to Fahrenheit.\n \t2. Convert from Fahrenheit to Celsius.\nEnter option: ");
-            int option = int.Parse(Console.ReadLine()!);
-    
-            switch(option)
-            {
-                case '1':
-                    Console.Write("Enter Celsius Value: ");
-                    double Celsius = double.Parse(Console.ReadLine()!);
-                    double Fahrenheit = (Celsius * (9/5)) + (double)32;
-                    Console.WriteLine($"Fahrenheit is : {Fahrenheit}");
-                    break;
-                case '2':
-                    Console.Write("Enter Fahrenheit Value: ");
-                    Fahrenheit = double.Parse(Console.ReadLine()!);
-                    Celsius = (Fahrenheit - 32) * 5/9;
-                    Console.WriteLine($"Celsius is: {Celsius}");
-                    break;
-            }
+            case 1:
+                Console.Write("Enter Celsius Value: ");
+                double Celsius = double.Parse(Console.ReadLine()!);
+                double Fahrenheit = (Celsius * (9/5)) + 32;
+                Console.WriteLine($"Fahrenheit is : {Fahrenheit}F");
+                Replay();
+                break;
+            case 2:
+                Console.Write("Enter Fahrenheit Value: ");
+                Fahrenheit = double.Parse(Console.ReadLine()!);
+                Celsius = (Fahrenheit - 32) * 5/9;
+                Console.WriteLine($"Celsius is: {Celsius} C");
+                Replay();
+                break;
+            case 3:
+                Console.WriteLine("Good bye !!!");
+                break;
+            default:
+                Console.WriteLine("Invalid Expression\nEnter a valid input.\n");
+                TemperatureConverter();
+            break;
         }
+    
+    
+}
+public static void Replay(){
+    Console.WriteLine("Do you wish want to perform another Conversion? (yes/no): ");
+    string respond = Console.ReadLine()!.ToLower();
+    switch (respond)
+    {
+        case "yes":
+            TemperatureConverter();
+        break;
+        case "no":
+            Console.WriteLine("Goodbye !!!");
+        return;
+        default:
+        Console.WriteLine("Choose an option:");
+        TemperatureConverter();
+        break;
     }
+}
+}
 }
